caa66f4d2163c9ffec63225d4fc6da9cf197d975
AbbasMahdavi021 <abbas.mahdavi021@gmail.com>
2023-06-16 01:17:01 -0700

    Completed Navbar, with side panel for smaller devices

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,16 @@
-import React from 'react';
-import { GiHamburgerMenu } from 'react-icons/gi';
-import { MdOutlineRestaurantMenu } from 'react-icons/md';
+"use client"
+
+import React, {useState} from 'react';
+import { RxEyeOpen } from 'react-icons/rx';
+import { RxEyeClosed } from 'react-icons/rx';
+
 import images from '@/constants/images';
 import './Navbar.css';
 
 const Navbar = () => {
+
+    const [toggleMenu, setToggleMenu] = useState(false);
+
     return (
         <nav className='app__navbar'>
             <div className='app__navbar-logo'>
@@ -18,6 +24,28 @@ const Navbar = () => {
                 <li className='p__opensans'><a href="#art">Concept Art</a></li>
                 <li className='p__opensans'><a href="#download">Download</a></li>
             </ul>
+
+            <div className="app__navbar-abbas">
+                <a href="https://abbasmahdavi.com" className="p__opensans" target='_blank'>By Abbas M</a>
+                <div />
+                <a href="https://github.com/AbbasMahdavi021/DOOM-Eternal-At-Home" className="p__opensans" target='_blank'>Github Repo</a>
+            </div>
+
+            <div className="app__navbar-smallscreen">
+                <RxEyeOpen color="#fff" fontSize={27} onClick={() => setToggleMenu(true)} />
+                {toggleMenu && (
+                    <div className="app__navbar-smallscreen_overlay flex__center slide-bottom">
+                        <RxEyeClosed fontSize={27} className="overlay__close" onClick={() => setToggleMenu(false)} />
+                        <ul className="app__navbar-smallscreen_links">
+                            <li><a href="#home" onClick={() => setToggleMenu(false)}>Home</a></li>
+                            <li><a href="#about" onClick={() => setToggleMenu(false)}>About</a></li>
+                            <li><a href="#menu" onClick={() => setToggleMenu(false)}>Menu</a></li>
+                            <li><a href="#awards" onClick={() => setToggleMenu(false)}>Awards</a></li>
+                            <li><a href="#contact" onClick={() => setToggleMenu(false)}>Contact</a></li>
+                        </ul>
+                    </div>
+                )}
+            </div>
         </nav>
     )
 }
