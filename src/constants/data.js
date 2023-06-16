8acd675673264256055ae5a8b05a66fbd15843cb
AbbasMahdavi021 <abbas.mahdavi021@gmail.com>
2023-06-16 07:05:18 -0700

    Remove unused data

--- src/constants/data.js
+++ src/constants/data.js
@@ -27,27 +27,4 @@ const assets = [
   }
 ];
 
-const awards = [
-  {
-    imgUrl: images.award02,
-    title: 'Bib Gourmond',
-    subtitle: 'Lorem ipsum dolor sit amet, consectetur.',
-  },
-  {
-    imgUrl: images.award01,
-    title: 'Rising Star',
-    subtitle: 'Lorem ipsum dolor sit amet, consectetur.',
-  },
-  {
-    imgUrl: images.award05,
-    title: 'AA Hospitality',
-    subtitle: 'Lorem ipsum dolor sit amet, consectetur.',
-  },
-  {
-    imgUrl: images.award03,
-    title: 'Outstanding Chef',
-    subtitle: 'Lorem ipsum dolor sit amet, consectetur.',
-  },
-];
-
 export default { technologies, assets, awards };
