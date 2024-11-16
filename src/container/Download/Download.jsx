e40bf042f4c1a756797c2f59fdad35134b394def
Abbas Mahdavi <abbas.mahdavi021@gmail.com>
2024-11-16 11:10:14 -0800

    Update Download

--- src/container/Download/Download.jsx
+++ src/container/Download/Download.jsx
@@ -25,7 +25,7 @@ const Download = () => {
                 <div className="app__wrapper-content">
                     <p className="p__opensans">Experience The Thrill Of DOOM Eternal At Home, A Game That Brings The Action-Packed Adventure To Your Home!</p>
                     <p className="p__cormorant" style={{ color: '#DCCA87', margin: '2rem 0' }}>Get The Game for FREE</p>
-                    <p className="p__opensans">Get Ready To Embark On An Epic Journey By Downloading The Game JAR File (91MB)</p>
+                    <p className="p__opensans">Get Ready To Embark On An Epic Journey By Downloading The Game JAR File (91MB), Requires JDK 17+ installed.</p>
                 </div>
                 <button onClick={handleDownloadJar} type="button" className="custom__button" style={{ marginTop: '2rem' }}>Download</button>
             </div>
